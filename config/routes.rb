53b9fc94bacad77d27474e917e8e13a13dd0d12b
Rafael Tejado <rtejado90@gmail.com>
2015-02-24 10:04:55 +0100

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   	resources :comments
   end
   resources :tags
-
+  resources :authors
+  resources :author_sessions, only: [:new, :create, :destroy]
+  
   root to: 'articles#index'
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
